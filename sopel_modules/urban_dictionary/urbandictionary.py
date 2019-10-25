2a2cbc58fdf776894f9a3beba84d6b2b99bef9a8
Jonathan Brannan <capsterx@hotmail.com>
2019-10-25 02:48:41 +0000

    Adding in some random lookups

--- sopel_modules/urban_dictionary/urbandictionary.py
+++ sopel_modules/urban_dictionary/urbandictionary.py
@@ -17,24 +17,39 @@ def ud_conv(s):
     newlines = re.sub(r"\r?\n(\r?\n)*", f" {BOLD}--{BOLD} ", italics)
     return newlines
 
-@sopel.module.commands('ud')
-@sopel.module.example('.ud netflix and chill')
-def urbandictionary_lookup(bot, trigger):
-    if not trigger.group(2):
+def urbandictionary_lookup(bot, nick, word):
+    if not word:
         bot.say(f"{trigger.nick} You must specify something to search")
         return
 
     defs = []
     try:
-        defs = ud.define(trigger.group(2))
+        defs = ud.define(word)
     except Exception as e:
-        bot.say(f"{trigger.nick} {e}")
+        bot.say(f"{nick} {e}")
         return
 
     if len(defs) == 0:
-        bot.say(f"{trigger.nick} no results found for {trigger.group(2)}")
+        bot.say(f"{nick} no results found for {word}")
         return
 
     defs.sort(key = lambda x: x.upvotes, reverse=True)
     d = defs[0]
     bot.say(f"{BOLD}{d.word}{BOLD}: {ud_conv(d.definition)} {BOLD}Example{BOLD}: {ud_conv(d.example)}", max_messages=3)
+
+@sopel.module.commands('ud')
+@sopel.module.example('.ud netflix and chill')
+def urbandictionary_lookup_cmd(bot, trigger):
+    return urbandictionary_lookup(bot, trigger.nick, trigger.group(2))
+
+@sopel.module.commands('udrw')
+@sopel.module.example('.udrw')
+def urbandictionary_random_words(bot, trigger):
+    bot.say(', '.join([x.word for x in ud.random()]))
+
+@sopel.module.commands('udr')
+@sopel.module.example('.udr')
+def urbandictionary_random(bot, trigger):
+    defs = ud.random()
+    defs.sort(key = lambda x: x.upvotes, reverse=True)
+    return urbandictionary_lookup(bot, trigger.nick, defs[0].word)
